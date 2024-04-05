640c68bb84aa8d59426cf3467e96c634a2532420
Markus Kohonen <6650964+maiger@users.noreply.github.com>
2024-04-05 15:15:48 +0300

    Add recipe content

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,11 @@ import { useRecipeStore } from "./stores/recipes";
 import { RouterView } from "vue-router";
 import Header from "@/components/Header.vue";
 import Footer from "@/components/Footer.vue";
-// import recipeData from "./assets/recipes.json";
+import recipeData from "@/assets/recipes.json";
 
 // Load recipes
 const recipeStore = useRecipeStore();
-// recipeStore.setRecipes(recipeData);
+recipeStore.setRecipes(recipeData);
 </script>
 
 <template>
