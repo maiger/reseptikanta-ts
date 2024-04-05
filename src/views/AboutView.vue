7d80e55a3ed33ad4007b463a929934a33165bb22
Markus Kohonen <6650964+maiger@users.noreply.github.com>
2024-04-05 14:22:44 +0300

    Add about page and route

---
+++ src/views/AboutView.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="about">
+    <div class="content">
+      <h1>Tervetuloa Reseptikantaan!</h1>
+      <p>Täältä löydät herkullisimmat reseptit jokaiseen tilanteeseen.</p>
+      <p>
+        Reseptit on koottu vuosien(kymmenien) saatossa useista eri lähteistä.
+      </p>
+      <p>
+        Reseptejä on koottu niin historian havinoista, kuin lehdista ja
+        internetin syövereistä.
+      </p>
+      <p>
+        Monia reseptejä on muokattu alkuperäisestä omien maukujen mukaisesti.
+      </p>
+      <h2>Ominaisuudet</h2>
+      <ul>
+        <li>Avoin hakukenttä</li>
+        <li>Haku avainsanoilla</li>
+        <li>Reseptien vaiheitten seuranta</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style>
+.about {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  .content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    padding: 4rem;
+
+    min-height: 80vh;
+    max-width: 80rem;
+    margin-top: 3rem;
+    border-radius: 20px;
+    color: var(--color-secondary-1-dark-1);
+    background-color: var(--color-primary-1-dark);
+  }
+
+  h2 {
+    margin: 3rem 0 0 0;
+  }
+
+  ul {
+    margin-top: 1rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+}
+</style>
