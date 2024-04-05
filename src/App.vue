cf580a1f85e33d5de63371c489189bae391a877d
Markus Kohonen <6650964+maiger@users.noreply.github.com>
2024-04-05 12:31:38 +0300

    Add header

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,43 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
-import RecipeView from "./views/RecipeView.vue";
+// import { useRecipeStore } from "./stores/recipes";
+import { RouterView } from "vue-router";
+import Header from "@/components/Header.vue";
+// import Footer from "./components/Footer.vue";
+// import recipeData from "./assets/recipes.json";
+
+// Load recipes
+// const recipeStore = useRecipeStore();
+// recipeStore.setRecipes(recipeData);
 </script>
 
 <template>
-  <div>Hello World</div>
-  <RecipeView></RecipeView>
+  <Header />
+  <router-view class="main-content" v-slot="{ Component }">
+    <transition name="scale" mode="out-in">
+      <component :is="Component" />
+    </transition>
+  </router-view>
+  <Footer />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<style scss scoped>
+.main-content {
+  /* Take the whole height minus header and footer heights */
+  min-height: calc(100vh - 7rem - 10rem);
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.scale-enter-active,
+.scale-leave-active {
+  transition: all 0.3s ease;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+.scale-enter-from {
+  opacity: 0;
+  transform: scale(1.05);
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.scale-leave-to {
+  opacity: 0;
+  transform: scale(0.95);
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,90 @@
+<template>
+  <header>
+    <nav class="container">
+      <RouterLink to="/">
+        <div class="branding">
+          <img src="../assets/logo.png" alt="Logo" width="48" height="48" />
+          <h1>Reseptikanta</h1>
+        </div>
+      </RouterLink>
+      <ul class="nav-routes">
+        <li>
+          <RouterLink to="/" aria-label="home">
+            <Icon class="icon" icon="material-symbols:home-outline" />
+          </RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/about" aria-label="about">
+            <Icon class="icon" icon="material-symbols:info-outline" />
+          </RouterLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script setup lang="ts">
+import { Icon } from "@iconify/vue";
+
+import { RouterLink } from "vue-router";
+</script>
+
+<style lang="scss" scoped>
+@import url("https://fonts.googleapis.com/css2?family=IBM+Plex+Serif:wght@400;500&display=swap");
+
+header {
+  background-color: var(--color-primary-1-dark);
+
+  nav {
+    display: flex;
+    height: 7rem;
+    align-items: center;
+    padding: 1rem 1.6rem;
+
+    .branding,
+    .nav-routes li .icon {
+      color: var(--color-secondary-1-dark-1);
+    }
+
+    .branding {
+      display: flex;
+      align-items: center;
+      gap: 0.8rem;
+      img {
+        max-width: 4rem;
+      }
+
+      h1 {
+        font-family: "IBM Plex Serif", serif;
+        font-size: var(--text-heading-size-1);
+        letter-spacing: 0.5px;
+      }
+    }
+
+    .nav-routes {
+      display: flex;
+      flex: 1;
+      justify-content: flex-end;
+      align-items: center;
+      gap: 1.2rem;
+      list-style: none;
+
+      li {
+        transition: all 0.2s;
+      }
+
+      li:hover {
+        transform: scale(1.1) rotateZ(-5deg);
+      }
+
+      .icon {
+        display: flex;
+        justify-content: center;
+        width: 3rem;
+        height: 3rem;
+        opacity: 0.9;
+      }
+    }
+  }
+}
+</style>
